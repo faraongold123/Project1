7a256da1c0ac247ce97a72720872d2f763406a59
Sergey <tiestoincontrol@gmail.com>
2020-12-12 15:18:57 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя', '');
+// answers[1] = prompt('Как ваша фамилия', '');
+// answers[2] = prompt('Сколько Вам лет', '');
+
+// document.write(answers);
+
+console.log(typeof(answers));
+console.log(typeof(null));
